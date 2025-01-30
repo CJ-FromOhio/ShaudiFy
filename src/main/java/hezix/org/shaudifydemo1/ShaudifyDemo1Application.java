fc32b0f90b3f340284319db12a374dc061a649fa
hezix <hezix@>
2025-01-30 19:48:32 +0300

    added git

---
+++ src/main/java/hezix/org/shaudifydemo1/ShaudifyDemo1Application.java
@@ -0,0 +1,13 @@
+package hezix.org.shaudifydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaudifyDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaudifyDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/hezix/org/shaudifydemo1/ShaudifyDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hezix.org.shaudifydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaudifyDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
